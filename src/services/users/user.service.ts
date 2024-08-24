b27861a3bf9e68f958137f172df8fbfea1a25622
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2024-08-25 01:17:25 +0500

    Resolved Incomming changes

--- src/services/users/user.service.ts
+++ src/services/users/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpService } from '../http/http.service';
 
 @Injectable({
@@ -9,9 +9,9 @@ export class UserService {
   constructor(private http: HttpService) {}
 
   checkUsernameExists(username: string): Observable<boolean> {
-    let data=this.http.getusername(username);
+    let data:any=this.http.getusername(username);
     console.log(data)
-    if(data.data.length=>0){
+    if(data.data.length >= 0){
       return of(true); // return an Observable that emits true
     } else {
       return of(false); // return an Observable that emits false
