92666271f365da91fdc26a021d4d2a635be51d78
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2024-09-09 16:23:01 +0500

    add Filters Functionality

--- src/app/views/website/second-header/second-header.component.ts
+++ src/app/views/website/second-header/second-header.component.ts
@@ -7,9 +7,15 @@ import { SearchServiceService } from 'src/services/search-service/search-service
   styleUrls: ['./second-header.component.css']
 })
 export class SecondHeaderComponent {
+  searchQuery: string = '';
+
   constructor(private searchService: SearchServiceService) {}
 
   onSearch(query: string) {
     this.searchService.changeSearchQuery(query);
+  }
+
+  onSearchChange() {
+    this.onSearch(this.searchQuery);
   }
 }
