3aa493c8b0836504b958ccbe8c2aa27020c63b1b
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2024-10-15 11:24:21 +0500

    Rsolved Bugs

--- src/app/views/website/people-syaing-component/people-syaing-component.component.ts
+++ src/app/views/website/people-syaing-component/people-syaing-component.component.ts
@@ -19,20 +19,20 @@ export class PeopleSyaingComponentComponent implements AfterViewInit {
       text: "You won’t regret it. Absolutely wonderful product!",
       rating: "⭐⭐⭐⭐⭐",
     },
-    {
-      name: "Megen W.",
-      role: "UI Designer",
-      image: "../../../../assets/images/megen-w.png",
-      text: "You won’t regret it. Absolutely wonderful product!",
-      rating: "⭐⭐⭐⭐⭐",
-    },
-    {
-      name: "Suzan B.",
-      role: "UI Designer",
-      image: "../../../../assets/images/suzan-b.png",
-      text: "You won’t regret it. Absolutely wonderful product!",
-      rating: "⭐⭐⭐⭐⭐",
-    }
+    // {
+    //   name: "Megen W.",
+    //   role: "UI Designer",
+    //   image: "../../../../assets/images/megen-w.png",
+    //   text: "You won’t regret it. Absolutely wonderful product!",
+    //   rating: "⭐⭐⭐⭐⭐",
+    // },
+    // {
+    //   name: "Suzan B.",
+    //   role: "UI Designer",
+    //   image: "../../../../assets/images/suzan-b.png",
+    //   text: "You won’t regret it. Absolutely wonderful product!",
+    //   rating: "⭐⭐⭐⭐⭐",
+    // }
   ];
 
   constructor(
