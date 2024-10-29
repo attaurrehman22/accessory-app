36471b658174dd5b0261152e7ef00bbd0883c0c1
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2024-10-29 23:17:35 +0500

    Create Popular Brands Component

--- src/app/views/website/brands-list/brands-list.component.ts
+++ src/app/views/website/brands-list/brands-list.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
+import { HttpService } from "src/services/http/http.service";
 
 @Component({
   selector: 'app-brands-list',
   templateUrl: './brands-list.component.html',
   styleUrls: ['./brands-list.component.css']
 })
-export class BrandsListComponent {
+export class BrandsListComponent implements OnInit {
+  selectedCategory: any;
+  showList: any[] = []; // Store products for the selected category
+  categoryNames: any[] = [];
+  commentsList: any;
 
+  changeCategory(category: any) {
+    this.selectedCategory = category;
+
+    this.showList = (category.products || []).map((product: any) => {
+      if (product.main_image) {
+        product.main_image = product.main_image.replace(/\\/g, ""); // Remove all backslashes
+      }
+      return product;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllTopNewArrivals();
+  }
+
+  getAllTopNewArrivals() {
+    this.http.getAllTopNewArrivalCategory().subscribe(
+      (res) => {
+        this.categoryNames = res.data; // Store the entire category data
+        this.selectedCategory = this.categoryNames[0]; // Set the first category as default
+        this.showList = this.selectedCategory.products || []; // Load products for the default category
+      },
+      (err) => {}
+    );
+  }
+
+  constructor(
+    public translateService: TranslateService,
+    private http: HttpService,
+    private router: Router
+  ) {
+    const supportedLanguages = ["en", "ar"];
+    this.translateService.addLangs(supportedLanguages);
+    this.translateService.setDefaultLang("en");
+
+    const browserLang = this.translateService.getBrowserLang();
+    if (supportedLanguages.includes(browserLang)) {
+      this.translateService.use(browserLang);
+    }
+  }
+
+  toggleHeart(watch): void {
+    // this.watches[index].liked = !this.watches[index].liked;
+  }
+
+  useLang(lang: string) {
+    this.translateService.use(lang);
+  }
+
+  routeToDetailPage(watch) {
+    this.router.navigate(["/buy-product"], {
+      state: { data: watch },
+    });
+  }
+
+  goToList(){
+    this.router.navigate(['/product-list'])
+  }
 }
+
