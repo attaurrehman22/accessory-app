869d9181e4a1a29de82d9f027e619c7981c982e2
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2024-11-05 15:27:11 +0500

    Set complete Prof Of Owner Ship

--- src/app/views/website/add-new-product/add-new-product.component.ts
+++ src/app/views/website/add-new-product/add-new-product.component.ts
@@ -111,6 +111,33 @@ export class AddNewProductComponent implements OnInit {
     return `${formattedHour}:${formattedMinute}`;
   }
 
+  
+  @ViewChild('fileInput0') fileInput0!: ElementRef<HTMLInputElement>;
+  @ViewChild('fileInput1') fileInput1!: ElementRef<HTMLInputElement>;
+  imagePreviews: string[] = ['', ''];
+  selectedFiles: File[] = [null, null];
+  triggerFileInput(clockIndex: number): void {
+    if (clockIndex === 0) {
+      this.fileInput0.nativeElement.click();
+    } else if (clockIndex === 1) {
+      this.fileInput1.nativeElement.click();
+    }
+  }
+
+  onFileSelected(event: Event, clockIndex: number): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      const file = fileInput.files[0];
+      this.selectedFiles[clockIndex] = file; // Store the selected file
+      console.log(`Selected file for clock ${clockIndex + 1}:`, file);
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imagePreviews[clockIndex] = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
 
   options = [
@@ -300,37 +327,6 @@ export class AddNewProductComponent implements OnInit {
           console.log(`No file selected for Clock ${index + 1}`);
         }
       });
-    }
-  }
-
-
-
-
-
-  @ViewChild('fileInput0') fileInput0!: ElementRef<HTMLInputElement>;
-  @ViewChild('fileInput1') fileInput1!: ElementRef<HTMLInputElement>;
-  imagePreviews: string[] = ['', ''];
-  selectedFiles: File[] = [null, null];
-  triggerFileInput(clockIndex: number): void {
-    if (clockIndex === 0) {
-      this.fileInput0.nativeElement.click();
-    } else if (clockIndex === 1) {
-      this.fileInput1.nativeElement.click();
-    }
-  }
-
-  onFileSelected(event: Event, clockIndex: number): void {
-    const fileInput = event.target as HTMLInputElement;
-    if (fileInput.files && fileInput.files[0]) {
-      const file = fileInput.files[0];
-      this.selectedFiles[clockIndex] = file; // Store the selected file
-      console.log(`Selected file for clock ${clockIndex + 1}:`, file);
-
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.imagePreviews[clockIndex] = e.target.result;
-      };
-      reader.readAsDataURL(file);
     }
   }
 
