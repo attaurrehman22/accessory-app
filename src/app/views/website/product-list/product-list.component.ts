768c1e8a6916f7bfc69f7957277751a502e62fdf
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2025-01-17 00:47:09 +0500

    Set Product Listing with filters

--- src/app/views/website/product-list/product-list.component.ts
+++ src/app/views/website/product-list/product-list.component.ts
@@ -11,121 +11,109 @@ import { SearchServiceService } from "src/services/search-service/search-service
   styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent implements OnInit {
-  supportLanguages = ["en", "ar", "fr", "ta", "hi"];
-  currentLanguage: string;
-  productsList: any;
-  title = "chronowatch";
-  currentPage: number = 1;
 
-  totalItems: number = 0;
-  itemsPerPage: number = 10;
-  totalPageNumbers: number[] = [];
-  itemsPerPageOptions: number[] = [10, 20, 50, 80];
-  brandsDataList: any;
-  category_sel_frm_popular_models: any;
-
-  searchQuery: string = "";
-  isCollapsed = false;
-  isCollapsed2 = false;
+  showList: any;
   isUserLogin: any;
+  watchTypes:any[]=[];
+  categories:any[]=[];
 
-  checkedBrands: any[] = [];
-  checkedCategories: any[] = [];
-
-  changePage(page: number) {
-    this.currentPage = page;
-    // Call your API or data-fetching logic here to fetch items for the current page
-    console.log(`Page changed to: ${page}`);
-    this.fetchProducts(); // Update products list based on new page
-  }
-
-  onItemsPerPageChange() {
-    this.currentPage = 1; // Reset to the first page
-    this.fetchProducts(); // Fetch products with updated items per page
-    console.log(`Items per page changed to: ${this.itemsPerPage}`);
-  }
-
-  fetchProducts() {
-    const queryString = this.buildQueryString();
-    // this.getProductsByCategory(queryString);
-  }
-
-  toggleCollapse() {
-    this.isCollapsed = !this.isCollapsed;
-  }
-
-  toggleCollapse2() {
-    this.isCollapsed2 = !this.isCollapsed2;
-  }
-
-  onCheckboxChange(event: Event, item: any, type: string) {
-    const isChecked = (event.target as HTMLInputElement).checked;
-    if (type === "brand") {
-      if (isChecked) {
-        this.checkedBrands.push(item);
-      } else {
-        this.checkedBrands = this.checkedBrands.filter(
-          (brand) => brand.id !== item.id
-        ); // Remove from checked list
-      }
-      console.log("Checked Brands:", this.checkedBrands);
-    } else if (type === "category") {
-      if (isChecked) {
-        this.checkedCategories.push(item); // Add to checked list
-      } else {
-        this.checkedCategories = this.checkedCategories.filter(
-          (category) => category.id !== item.id
-        ); // Remove from checked list
-      }
-      console.log("Checked Categories:", this.checkedCategories);
+  ngOnInit(): void {
+    this.getAllCategories()
+    this.getAllBrands()
+    this.isUserLogin = localStorage.getItem("isLoggedIn");
+    if (this.isUserLogin === "true") {
+      this.getWishList();
     }
-
-    this.buildQueryString();
+    this.applyFilters();
+    // this.getAllModels();
   }
 
-  buildQueryString() {
-    let queryString = "";
+  wishList: any;
+  getWishList() {
+    this.http.getWishList().subscribe((res) => {
+      this.wishList = res.data;
+    });
+  }
 
-    // Add categories to the query string
-    if (this.checkedCategories.length > 0) {
-      this.checkedCategories.forEach((id, index) => {
-        queryString += `category_ids[]=${id.id}`;
-        if (index < this.checkedCategories.length - 1) {
-          queryString += "&"; // Add an "&" if it's not the last element
+  addWishList(watch) {
+    const formData = {
+      product_id: watch.id,
+    };
+    this.http.addWishList(formData).subscribe((res) => {
+      this.wishList = res.data;
+      this.getWishList();
+      this.applyFilters();
+      // this.getAllModels();
+    });
+  }
+
+  getAllCategories() {
+    this.http.getCategoryDropDown().subscribe(
+      (res) => {
+        this.categories = res.data;
+      },
+      (err) => {}
+    );
+  }
+
+  clearFilters(){
+    this.categories.forEach(category => (category.selected = false));
+    this.watchTypes.forEach(type => (type.selected = false));
+    this.priceRange = { from: 200, to: 50000 };
+    this.applyFilters();
+  }
+
+
+  applyFilters() {
+    console.log('Selected Categories:', this.categories.filter(c => c.selected));
+    console.log('Selected Watch Types:', this.watchTypes.filter(w => w.selected));
+    console.log('Price Range:', this.priceRange);
+    let queryString = "";
+    // // Add categories to the query string
+    if (this.categories.length > 0) {
+      this.categories.forEach((id, index) => {
+        console.log("Category ID ------ ",id)
+        if(id.selected){
+          queryString += `category_ids[]=${id.id}`;
+          if (index < this.categories.length - 1) {
+            queryString += "&"; // Add an "&" if it's not the last element
+          }
         }
       });
     }
-
-    // Add brands to the query string
-    if (this.checkedBrands.length > 0) {
+    // // Add brands to the query string
+    if (this.watchTypes.length > 0) {
       if (queryString) {
         queryString += "&"; // Add '&' if categories already exist
       }
-      this.checkedBrands.forEach((id, index) => {
-        queryString += `brand_ids[]=${id.id}`; // Use brand_ids[] to store as an array
-        if (index < this.checkedBrands.length - 1) {
-          queryString += "&"; // Add '&' between values, not at the end
+      this.watchTypes.forEach((id, index) => {
+        console.log("Brand ID ------ ",id)
+        if(id.selected){
+          queryString += `brand_ids[]=${id.id}`; // Use brand_ids[] to store as an array
+          if (index < this.watchTypes.length - 1) {
+            queryString += "&"; // Add '&' between values, not at the end
+          }
         }
       });
     }
+    let currentPage=1;
+    let itemsPerPage=100;
+    queryString += `&page=${currentPage}`;
+    queryString += `&per_page=${itemsPerPage}`;
 
-    queryString += `&page=${this.currentPage}`;
-    queryString += `&per_page=${this.itemsPerPage}`;
-
-    // Call the API with the constructed query string
+    // // Call the API with the constructed query string
     if (queryString) {
       this.getProductsByCategory(queryString);
     }
   }
 
-  // Method to call the API with the generated query string
   getProductsByCategory(queryString: any) {
     this.http.getProductsByCategory(queryString).subscribe((res) => {
-      this.productsList = res;
-      this.totalItems = this.productsList.data.last_page;
-      this.currentPage = this.productsList.data.current_page;
-      this.productsList = this.productsList.data.data;
-      this.productsList.data.data.map((product: any) => {
+      this.showList = res;
+      // this.totalItems = this.productsList.data.last_page;
+      // this.currentPage = this.productsList.data.current_page;
+      this.showList = this.showList.data.data;
+      this.showList.data.data.map((product: any) => {
         if (product.main_image) {
           product.main_image = product.main_image
             .replace(/\\/g, "/")
@@ -149,99 +137,107 @@ export class ProductListComponent implements OnInit {
         }
         return product;
       });
+
+      console.log("this.showList",this.showList)
     });
   }
 
-  ngOnInit(): void {
-    this.isUserLogin = localStorage.getItem("isLoggedIn");
-    if (this.isUserLogin === "true") {
-      this.getWishList();
-    }
-    this.getAllCategories();
-    this.getAllBrands();
-    this.fetchProducts();
-  }
 
-  brandsDropDownList: any;
+  // buildQueryString() {
+  //   let queryString = "";
+
+  //   // Add categories to the query string
+  //   if (this.checkedCategories.length > 0) {
+  //     this.checkedCategories.forEach((id, index) => {
+  //       queryString += `category_ids[]=${id.id}`;
+  //       if (index < this.checkedCategories.length - 1) {
+  //         queryString += "&"; // Add an "&" if it's not the last element
+  //       }
+  //     });
+  //   }
+
+  //   // Add brands to the query string
+  //   if (this.checkedBrands.length > 0) {
+  //     if (queryString) {
+  //       queryString += "&"; // Add '&' if categories already exist
+  //     }
+  //     this.checkedBrands.forEach((id, index) => {
+  //       queryString += `brand_ids[]=${id.id}`; // Use brand_ids[] to store as an array
+  //       if (index < this.checkedBrands.length - 1) {
+  //         queryString += "&"; // Add '&' between values, not at the end
+  //       }
+  //     });
+  //   }
+
+  //   queryString += `&page=${this.currentPage}`;
+  //   queryString += `&per_page=${this.itemsPerPage}`;
+
+  //   // Call the API with the constructed query string
+  //   if (queryString) {
+  //     this.getProductsByCategory(queryString);
+  //   }
+  // }
 
   getAllBrands() {
     this.http.getBrandsDropDownFilter().subscribe(
       (res) => {
-        this.brandsDataList = res.data;
+        this.watchTypes = res.data;
       },
       (err) => {}
     );
   }
 
-  showAll: boolean = false;
-
-  toggleSeeAll() {
-    this.showAll = !this.showAll;
-  }
-
-  showAll2: boolean = false;
-
-  toggleSeeAll2() {
-    this.showAll2 = !this.showAll2;
-  }
-
-  getAllCategories() {
-    this.http.getCategoryDropDown().subscribe(
-      (res) => {
-        this.category_sel_frm_popular_models = res.data;
-      },
-      (err) => {}
-    );
-  }
+  // getAllModels() {
+  //   this.http.getAllPopularModels().subscribe(
+  //     (res) => {
+  //       this.showList = res.data.map((model) => {
+  //         return {
+  //           ...model,
+  //           main_image: model.main_image
+  //             ? model.main_image.replace(/\\/g, "")
+  //             : null,
+  //         };
+  //       });
+  //     },
+  //     (err) => {
+  //       console.error(err);
+  //     }
+  //   );
+  // }
 
   constructor(
-    private http: HttpService,
-    private router: Router,
     public translateService: TranslateService,
-    private searchService: SearchServiceService,
-    private languageService: LanguageService
+    private http: HttpService,
+    private router: Router
   ) {
-    this.translateService.addLangs(this.supportLanguages);
-    const savedLang = this.languageService.getCurrentLanguage();
-    if (this.supportLanguages.includes(savedLang)) {
-      this.translateService.use(savedLang);
-    } else {
-      const browserLang = this.translateService.getBrowserLang();
-      this.currentLanguage = browserLang;
+    const supportedLanguages = ["en", "ar"];
+    this.translateService.addLangs(supportedLanguages);
+    this.translateService.setDefaultLang("en");
 
-      if (this.supportLanguages.includes(browserLang)) {
-        this.translateService.use(browserLang);
-        this.languageService.setLanguage(browserLang); // Save browser language if valid
-      }
+    const browserLang = this.translateService.getBrowserLang();
+    if (supportedLanguages.includes(browserLang)) {
+      this.translateService.use(browserLang);
     }
   }
 
-  currentIndex1: number = 0;
-  itemsPerPage1: number = 4;
+  useLang(lang: string) {
+    this.translateService.use(lang);
+  }
 
-  routeTo(slide) {
+  routeToDetailPage(watch) {
     this.router.navigate(["/buy-product"], {
-      state: { data: slide },
+      state: { data: watch },
     });
   }
 
-  wishList: any;
-  getWishList() {
-    this.http.getWishList().subscribe((res) => {
-      this.wishList = res.data;
-    });
+  priceRange = { from: 200, to: 50000 };
+
+ 
+
+  isShowFilters:boolean=false;
+
+  openFilters(){
+   this.isShowFilters=!this.isShowFilters
   }
 
-  addWishList(watch: any, event: MouseEvent) {
-    event.stopPropagation();
-    event.preventDefault();
-    const formData = {
-      product_id: watch.id,
-    };
-    this.http.addWishList(formData).subscribe((res) => {
-      this.wishList = res.data;
-      this.getWishList();
-      this.fetchProducts();
-    });
-  }
 }
