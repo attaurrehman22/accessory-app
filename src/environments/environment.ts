d2e579067172ae3c2ad2e971800d8f3e7caae58f
Muhammad Atta-ur-Rehman Shah <125760299+attaurrehman22@users.noreply.github.com>
2025-07-03 22:55:55 +0500

    Add Staging Base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apipath:"https://demo-api.chronosouq.com",
+  apipath:"https://api.chronosouq.com",
   production: false,
   firebase: {
     apiKey: "YOUR_API_KEY",
