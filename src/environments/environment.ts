4a465cabe3d66115504093bf128ede278a23d1ee
attaurrehmanshah22 <attaurrehmanshah22@gmail.com>
2026-02-17 12:05:03 -0800

    change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apipath: "https://api.chronosouq.com",
+  apipath: "https://pre-demo.chronosouq.com",
   production: false,
   firebase: {
     apiKey: "YOUR_API_KEY",
